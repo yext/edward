452137e165fe40763b047dc48c5adca5e5de8c04
Tom Elliott <telliott@yext.com>
2016-03-22 18:57:42 -0400

    Add config structs and stub for loading config prior to each command

--- main.go
+++ main.go
@@ -6,6 +6,50 @@ import (
 	"github.com/codegangsta/cli"
 )
 
+var groups []ServiceGroupConfig
+var services []ServiceConfig
+
+type ServiceConfigFile struct {
+	Services []ServiceConfig
+	Groups   []ServiceGroupConfig
+}
+
+// ServiceGroupConfig is a group of services that can be managed together
+type ServiceGroupConfig struct {
+	// A name for this group, used to identify it in commands
+	Name string
+	// Paths to child service config files
+	ServicePaths []string
+	// Full services contained within this group
+	Services []*ServiceConfig
+}
+
+// ServiceConfig represents a service that can be managed by Edward
+type ServiceConfig struct {
+	// Service name, used to identify in commands
+	Name string
+	// Optional path to service. If nil, uses cwd
+	Path *string
+	// Commands for managing the service
+	Commands struct {
+		// Command to build
+		Build string
+		// Command to launch
+		Launch string
+	}
+	// Service state properties that can be obtained from logs
+	Properties struct {
+		// Regex to detect a line indicating the service has started successfully
+		Started string
+		// Custom properties, mapping a property name to a regex
+		Custom map[string]string
+	}
+}
+
+func loadConfig() {
+	// TODO: Load configuration from the config file and populate the service and groups variables
+}
+
 func list(c *cli.Context) {
 	println("List services")
 }
@@ -39,9 +83,15 @@ func log(c *cli.Context) {
 }
 
 func main() {
+
 	app := cli.NewApp()
 	app.Name = "Edward"
 	app.Usage = "Manage local microservices"
+	app.Before = func(c *cli.Context) error {
+		loadConfig()
+		println("Before")
+		return nil
+	}
 	app.Commands = []cli.Command{
 		{
 			Name:   "list",
