6a140644136363a9cc3679756ad7b93564402b97
Tom Elliott <tom.w.elliott@gmail.com>
2016-10-30 23:12:38 -0400

    Fix unit tests following watcher formatting changes.

--- generators/generators_test.go
+++ generators/generators_test.go
@@ -21,10 +21,10 @@ var goTests = []struct {
 		path: "testdata/go_simple/",
 		outServices: []*services.ServiceConfig{
 			{
-				Name:  "simple",
-				Path:  common.StringToStringPointer("gocode/src/yext/simple"),
-				Env:   []string{},
-				Watch: common.StringToStringPointer("gocode/src/yext/simple"),
+				Name:      "simple",
+				Path:      common.StringToStringPointer("gocode/src/yext/simple"),
+				Env:       []string{},
+				WatchJson: []byte("{\"include\":[\"gocode/src/yext/simple\"]}"),
 				Commands: services.ServiceConfigCommands{
 					Build:  "go install",
 					Launch: "simple",
@@ -38,20 +38,20 @@ var goTests = []struct {
 		path: "testdata/go_multiple/",
 		outServices: []*services.ServiceConfig{
 			{
-				Name:  "service1",
-				Path:  common.StringToStringPointer("service1"),
-				Env:   []string{},
-				Watch: common.StringToStringPointer("service1"),
+				Name:      "service1",
+				Path:      common.StringToStringPointer("service1"),
+				Env:       []string{},
+				WatchJson: []byte("{\"include\":[\"service1\"]}"),
 				Commands: services.ServiceConfigCommands{
 					Build:  "go install",
 					Launch: "service1",
 				},
 			},
 			{
-				Name:  "service2",
-				Path:  common.StringToStringPointer("service2"),
-				Env:   []string{},
-				Watch: common.StringToStringPointer("service2"),
+				Name:      "service2",
+				Path:      common.StringToStringPointer("service2"),
+				Env:       []string{},
+				WatchJson: []byte("{\"include\":[\"service2\"]}"),
 				Commands: services.ServiceConfigCommands{
 					Build:  "go install",
 					Launch: "service2",
@@ -66,10 +66,10 @@ var goTests = []struct {
 		targets: []string{"service1"},
 		outServices: []*services.ServiceConfig{
 			{
-				Name:  "service1",
-				Path:  common.StringToStringPointer("service1"),
-				Env:   []string{},
-				Watch: common.StringToStringPointer("service1"),
+				Name:      "service1",
+				Path:      common.StringToStringPointer("service1"),
+				Env:       []string{},
+				WatchJson: []byte("{\"include\":[\"service1\"]}"),
 				Commands: services.ServiceConfigCommands{
 					Build:  "go install",
 					Launch: "service1",
