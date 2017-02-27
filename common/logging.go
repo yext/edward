8563fac4f1d8c011e467fe8cb80ae892fdf114e8
Tom Elliott <tom.w.elliott@gmail.com>
2017-02-26 22:01:01 -0500

    Fix lint warnings in common package

--- common/logging.go
+++ common/logging.go
@@ -1,13 +1,18 @@
 package common
 
+// Logger provides a simple logging interface with minimal print function(s)
 type Logger interface {
 	Printf(format string, v ...interface{})
 }
 
+// NullLogger implements the Logger interface with no-op functions
 type NullLogger struct{}
 
+// Printf is a no-op print function
 func (n NullLogger) Printf(_ string, _ ...interface{}) {}
 
+// MaskLogger takes a Logger and returns the Logger if not nil, or a NullLogger
+// if it is nil.
 func MaskLogger(logger Logger) Logger {
 	if logger != nil {
 		return logger
--- common/test_utils.go
+++ common/test_utils.go
@@ -1,5 +1,6 @@
 package common
 
+// StringToStringPointer converts a string value to a string pointer
 func StringToStringPointer(s string) *string {
 	return &s
 }
