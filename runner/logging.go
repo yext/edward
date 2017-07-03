02e9502feae6da331ab9c0a09017decf821c2d3f
Tom Elliott <tom.w.elliott@gmail.com>
2017-07-03 15:16:35 -0400

    Refactor logging related code in the runner to logging.go.

--- runner/logging.go
+++ runner/logging.go
@@ -2,6 +2,9 @@ package runner
 
 import (
 	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -23,4 +26,41 @@ func ParseLogLine(line string) (LogLine, error) {
 		return LogLine{}, errors.WithStack(err)
 	}
 	return lineData, nil
+}
+
+// Log provides the io.Writer interface to publish service logs to file
+type Log struct {
+	file   *os.File
+	name   string
+	stream string
+}
+
+// Printf prints a message to a RunnerLog
+func (r *Log) Printf(format string, a ...interface{}) {
+	msg := fmt.Sprintf(format, a...)
+	r.Write([]byte(msg))
+}
+
+// Write writes a slice of bytes to a RunnerLog
+func (r *Log) Write(p []byte) (int, error) {
+	fmt.Println(strings.TrimRight(string(p), "\n"))
+	lineData := LogLine{
+		Name:    r.name,
+		Time:    time.Now(),
+		Stream:  r.stream,
+		Message: strings.TrimSpace(string(p)),
+	}
+
+	jsonContent, err := json.Marshal(lineData)
+	if err != nil {
+		return 0, errors.Wrap(err, "could not prepare log line")
+	}
+
+	line := fmt.Sprintln(string(jsonContent))
+	count, err := r.file.Write([]byte(line))
+	if err != nil {
+		fmt.Println("Error")
+		return count, errors.Wrap(err, "could not write log line")
+	}
+	return len(p), nil
 }
--- runner/runner.go
+++ runner/runner.go
@@ -1,12 +1,9 @@
 package runner
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
 	"os/exec"
 	"os/signal"
-	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -282,41 +279,4 @@ func (r *Runner) startService() error {
 	r.command.Start(errorLog)
 
 	return nil
-}
-
-// Log provides the io.Writer interface to publish service logs to file
-type Log struct {
-	file   *os.File
-	name   string
-	stream string
-}
-
-// Printf prints a message to a RunnerLog
-func (r *Log) Printf(format string, a ...interface{}) {
-	msg := fmt.Sprintf(format, a...)
-	r.Write([]byte(msg))
-}
-
-// Write writes a slice of bytes to a RunnerLog
-func (r *Log) Write(p []byte) (int, error) {
-	fmt.Println(strings.TrimRight(string(p), "\n"))
-	lineData := LogLine{
-		Name:    r.name,
-		Time:    time.Now(),
-		Stream:  r.stream,
-		Message: strings.TrimSpace(string(p)),
-	}
-
-	jsonContent, err := json.Marshal(lineData)
-	if err != nil {
-		return 0, errors.Wrap(err, "could not prepare log line")
-	}
-
-	line := fmt.Sprintln(string(jsonContent))
-	count, err := r.file.Write([]byte(line))
-	if err != nil {
-		fmt.Println("Error")
-		return count, errors.Wrap(err, "could not write log line")
-	}
-	return len(p), nil
 }
