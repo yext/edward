8edae99ece41585ec8a63dd2d6099ea93350f53d
Matt Kilpatrick <mkilpatrick@yext.com>
2019-11-27 11:52:36 -0500

    Revert "The current ParseCommand assumes that only one command and single"
    This reverts commit efe2c4ad3926809243031d569f34630c3e63c22e.

--- services/backends/commandline/buildandrun.go
+++ services/backends/commandline/buildandrun.go
@@ -16,7 +16,6 @@ import (
 	"github.com/theothertomelliott/gopsutil-nocgo/process"
 	"github.com/yext/edward/commandline"
 	"github.com/yext/edward/services"
-	"github.com/ebuchman/go-shell-pipes"
 )
 
 type buildandrun struct {
@@ -181,16 +180,19 @@ func (b *buildandrun) Stop(workingDir string, getenv func(string) string) ([]byt
 
 	var out []byte
 	if b.Backend.Commands.Stop != "" {
-        cmdTokens := commandline.TokenizeCommand(b.Backend.Commands.Stop)
-        byteString, err := pipes.RunStrings(cmdTokens...)
-
+		cmd, err := commandline.ConstructCommand(workingDir, b.Service.Path, b.Backend.Commands.Stop, getenv)
 		if err != nil {
-			return []byte(byteString), errors.WithStack(err)
+			return nil, errors.WithStack(err)
 		}
 
-        if b.waitForCompletionWithTimeout(1 * time.Second) {
-            return []byte(byteString), nil
-        }
+		out, err = cmd.CombinedOutput()
+		if err != nil {
+			return out, errors.WithStack(err)
+		}
+
+		if b.waitForCompletionWithTimeout(1 * time.Second) {
+			return out, nil
+		}
 	}
 
 	err := InterruptGroup(b.cmd.Process.Pid, b.Service)
